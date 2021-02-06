2f3e9db7d1ec81c1a24c6633ce6a6b6c66bb5ac8
Simone Cavallini <33960860+SimoCava@users.noreply.github.com>
2021-02-06 14:25:38 +0100

    Added EasyBanks project

---
+++ Easybank landing page/app/js/script.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  // Close hamburger menu
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  }
+  // Open hamburger menu
+  else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
